0d7ca7e800e98098be45d17c11b94b3ff1b10050
Nikhil Kotian <nix220287@gmail.com>
2014-09-19 20:53:37 +0100

    Initial addition

---
+++ src/FlyWithWings.cpp
@@ -0,0 +1,19 @@
+/*
+ * FlyWithWings.cpp
+ *
+ *  Created on: Sep 18, 2014
+ *      Author: Nikhil
+ */
+
+#include "FlyWithWings.h"
+#include "iostream"
+using namespace std;
+
+FlyWithWings::FlyWithWings() {
+
+}
+
+void FlyWithWings::fly()
+{
+	cout<< "I fly with wings"<<endl;
+}
