29658fba6d0f0a44d6e3e1b02e645eefd199c20c
Nikhil Kotian <nix220287@gmail.com>
2014-09-29 22:25:16 +0100

    Factory directory created.

---
+++ Factory/PizzaStore.cpp
@@ -0,0 +1,18 @@
+/*
+ * PizzaStore.cpp
+ *
+ *  Created on: Sep 29, 2014
+ *      Author: Nikhil
+ */
+
+#include "C:PizzaStore.h"
+
+PizzaStore::PizzaStore() {
+	// TODO Auto-generated constructor stub
+
+}
+
+PizzaStore::~PizzaStore() {
+	// TODO Auto-generated destructor stub
+}
+
